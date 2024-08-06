4d3cccfd56248e2ecc428ea09fb26a37f659abb0
Harsh Jain <91270740+Harshj054@users.noreply.github.com>
2024-08-06 14:40:45 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function ver_idf() {
+    local_version=0.0.0
+
+    latest_version=$(curl -s https://api.github.com/repos/Harshj054/test/releases/latest | grep '"tag_name":' | cut -d '"' -f 4)
+
+    if [ "$local_version" != "$latest_version" ]; then
+        echo "Your version ($local_version) is outdated. The latest version is $latest_version."
+        read -p "Do you want to update to the latest version? (y/n): " choice
+        if [ "$choice" == "y" ]; then
+            echo "Updating to version $latest_version..."
+            git clone https://github.com/Harshj054/test.git
+            echo "Update completed."
+        else
+            echo "Update canceled."
+        fi
+    else
+        echo "You are using the latest version ($local_version)."
+        local_version=$latest_version
+    fi
+}
+
+ver_idf
